e19cb40c133170197db5568ffe590d1635d83df3
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-04-14 14:15:13 -0300

    Alterations in main function of __init__.py

--- Cryptography.py
+++ Cryptography.py
@@ -2,41 +2,36 @@ from MatFunctions import MatFunctions
 from Keys import Keys
 
 
-class Cryptography:
+class Encrypt:
     def __init__(self, p, q, a, b):
         self.n = p * q
         self.phi = MatFunctions.phi(p, q)
         self.e = Keys.public(self.phi, self.n)
         self.d = Keys.private(self.e, self.phi, a, b)
 
-
-class Encrypt(Cryptography):
     def text(self, string):
-        if type(string) != "<class 'str'>":
+        if type(string) != str:
             return -1
 
         chars = list(string)
         new_encrypt = []
-        j = 0
         for i in chars:
-            new_encrypt[j] = int(i) ** self.e
-            new_encrypt[j] %= self.n
-            j += 1
+            new_encrypt.append((ord(i) ** self.e) % self.n)
 
         return new_encrypt
 
 
-class Decrypt(Cryptography):
-    def text(self, string):
-        if type(string) != "<class 'str'>":
+class Decrypt:
+    def __init__(self, n, d):
+        self.n = n
+        self.d = d
+
+    def text(self, dec_list):
+        if type(dec_list) != "<class 'list'>":
             return -1
 
-        chars = list(string)
         new_encrypt = []
-        j = 0
-        for i in chars:
-            new_encrypt[j] = int(i) ** self.d
-            new_encrypt[j] %= self.n
-            j += 1
+        for i in dec_list:
+            new_encrypt.append(str((i ** self.d) % self.n))
 
         return new_encrypt
--- __init__.py
+++ __init__.py
@@ -1,37 +1,37 @@
-from Cryptography import Cryptography
+from Cryptography import Encrypt
+from Cryptography import Decrypt
+import csv
 
 
 def main():
     go = int(input("\nYou want:\n\t0:Encrypt\n\t1: Decrypt\n\tYour answer: "))
 
     if go == 0:
-        work = int(input("\nYou want to work with:\n\t0: Files\n\t1:Text\n\tYour answer: "))
-
-        if work == 0:
-            file = input("\nWhich file do you want to encrypt?\t(Remember for a extension)\n")
-        elif work == 1:
-            text = input("\nEnter the text: ")
-        else:
-            print("\nEnter a valid option!")
-
-            return -1
-
+        txt = input("\nEnter the text: ")
         print("\nEnter with two number primers (p and q): ")
-        p = int(input("     P: "))
-        q = int(input("     Q: "))
+        p = int(input("\tP: "))
+        q = int(input("\tQ: "))
+        encrypt = Encrypt(p, q, 100, 200)
+        csv_out = encrypt.text(txt)
 
-        crypto = Cryptography(p, q, 100, 200)
-
-        if work == 0:
-            pass
-        elif work == 1:
-            crypto.Encrypt.text(text)
-
+        with open(file='encrypt.csv', mode='w') as file:
+            writer = csv.writer(file)
+            writer.writecolumns(str(i) for i in csv_out)
     elif go == 1:
-        file = input("Which file do you want to decrypt?\t (Remember for a extension it's (.enc)\n")
+        file_name = input("Which file do you want to decrypt?\t (Remember for a extension it's (.csv)\n\tYour answer: ")
+        print("\nEnter with the keys (n and d): ")
+
+        with open(file=file_name, mode='r') as file:
+            read = csv.reader(file)
+
+        n = int(input("\tn: "))
+        e = int(input("\te: "))
+        decrypt = Decrypt(n, e)
+        string_decry = ''.join(str(i) for i in decrypt.text(read))
+
+        print(f"Your text: {string_decry}")
     else:
         print("Enter a valid option!")
-
         return -1
 
     return 1
